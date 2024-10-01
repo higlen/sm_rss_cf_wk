138ecc601047cf01887335f2e07f9a4d0d188d68
higlen <zhuglen@gmail.com>
2024-10-01 21:17:02 +0800

    Update route.js

--- src/route.js
+++ src/route.js
@@ -4,11 +4,11 @@ import bilibili_user_video from './lib/bilibili/user/video';
 import telegram_channel from './lib/telegram/channel';
 import weibo_user from './lib/weibo/user';
 import xiaohongshu_user from './lib/xiaohongshu/user';
-import annas from './lib/annas/annas';
+import annas_annas from './lib/annas/annas';
 
 const route = new Hono();
 
-let plugins = [bilibili_user_dynamic, bilibili_user_video, telegram_channel, weibo_user, xiaohongshu_user, annas];
+let plugins = [bilibili_user_dynamic, bilibili_user_video, telegram_channel, weibo_user, xiaohongshu_user, annas_annas];
 
 for (let plugin of plugins) {
 	plugin.setup(route);
